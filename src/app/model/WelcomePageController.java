832228b7dffbbbf955d2b93d38c6df1c3fa7b6f0
Mantas Astrauskas <37397172+MAstrauskas@users.noreply.github.com>
2018-11-17 07:00:52 +0000

    Created Welcome page and set up the MainApp class

---
+++ src/app/model/WelcomePageController.java
@@ -0,0 +1,41 @@
+package app.model;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+public class WelcomePageController {
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private FlowPane flowPane;
+
+    @FXML
+    private Text instructionsText;
+
+    @FXML
+    private Pane buttonPane;
+
+    @FXML
+    private Text welcomeText;
+
+    @FXML
+    void e15454(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleNextScene(ActionEvent event) {
+
+    }
+
+}
